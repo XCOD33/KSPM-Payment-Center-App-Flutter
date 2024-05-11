b23377ae0f2a6f719b4463362054af382e818b86
XCOD33 <denistraju155@gmail.com>
2024-05-11 19:03:07 +0700

    feat: add pembayaran list
    Add a list of pembayaran data to the pembayaranku page.
    
    The data includes title, description, price, and status.
    
    BREAKING CHANGE: The old pembayaran data has been replaced with the new data.

--- lib/pembayaranku.dart
+++ lib/pembayaranku.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:kspm_payment_center_app/utils/colors.dart';
 import 'package:kspm_payment_center_app/utils/text_style.dart';
 
@@ -14,6 +15,37 @@ class Pembayaranku extends StatefulWidget {
 }
 
 class _PembayarankuState extends State<Pembayaranku> {
+  var pembayaranku = [
+    {
+      "title": "Pembayaran Kunjungan Industri 1",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac nunc purus. Quisque cursus ipsum sit amet pellentesque aliquet. Integer eu posuere justo. Sed posuere tempus orci eu tristique. Nunc eget lorem a libero elementum porta. Donec euismod enim in est bibendum, ut varius orci placerat. Sed sit amet volutpat quam.",
+      "price": "Rp 150.000",
+      "status": "Lunas",
+    },
+    {
+      "title": "Pembayaran Kunjungan Industri 2",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porttitor dictum elit, vel sagittis eros consequat in. Fusce vitae lectus quam. Cras posuere neque eget faucibus aliquet. Etiam dignissim libero facilisis quam euismod, ac rhoncus tortor hendrerit. Fusce ultricies, risus sit amet maximus pharetra, enim ipsum pharetra dolor, sit amet vehicula mauris mauris eget libero. Quisque rhoncus dolor sem, et commodo est ultricies eu. Morbi ac nisi aliquet, sagittis orci non, accumsan elit. Aliquam congue leo sit amet venenatis tempus. Duis sollicitudin id dui nec consectetur. Fusce laoreet nisl eros, nec condimentum nisl commodo at. In semper tempus justo at tristique. Pellentesque malesuada iaculis aliquet. In id elementum quam. Aenean vulputate magna vel egestas finibus.",
+      "price": "Rp 170.000",
+      "status": "Belum Lunas",
+    },
+    {
+      "title": "Pembayaran Kunjungan Industri 3",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lacinia ligula condimentum laoreet tristique. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque pretium dui ac purus vehicula, sit amet mollis justo luctus. Fusce lorem risus, commodo eu mi in, rutrum malesuada mauris. Maecenas et dui pellentesque, pellentesque elit vel, pretium eros. Aliquam ac tortor iaculis, egestas nibh ut, finibus urna. Donec feugiat nibh sit amet velit molestie auctor. Nulla iaculis lectus libero. Ut at magna ac turpis pulvinar facilisis.",
+      "price": "Rp 190.000",
+      "status": "Belum Lunas",
+    },
+    {
+      "title": "Pembayaran Kunjungan Industri 4",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consectetur convallis nunc nec dapibus. Integer quis turpis id ex cursus posuere. Nullam sem ex, cursus in rhoncus non, vehicula et tellus. Phasellus vulputate laoreet magna, a dapibus tortor mattis a. Mauris et egestas mi. Donec vitae vulputate mauris. Nam in scelerisque neque. Cras consectetur quam ut aliquam hendrerit.",
+      "price": "Rp 100.000",
+      "status": "Lunas",
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -38,47 +70,62 @@ class _PembayarankuState extends State<Pembayaranku> {
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: 4,
-              itemBuilder: (context, index) {
+              itemCount: pembayaranku.length,
+              itemBuilder: (context, int index) {
                 return Card(
                   color: AppColors.lavenderMist,
                   margin: EdgeInsets.only(bottom: 15),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: ListTile(
-                      title: Padding(
-                        padding: EdgeInsets.only(bottom: 4),
-                        child: Text(
-                          "Pembayaran Kunjungan Industri 1",
-                          style: AppTextStyle.fontRegular(14),
-                        ),
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Lorem ipsum dolor sit amet consectetur. Etiam egestas ridiculus orci orci quam eu nisi. Aliquet enim diam risus rhoncus fermentum placerat. Sapien ipsum ornare ac accumsan est amet ut.",
-                            style: AppTextStyle.fontRegular(12),
+                  child: InkWell(
+                    onTap: () {
+                      print(pembayaranku);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: ListTile(
+                        title: Padding(
+                          padding: EdgeInsets.only(bottom: 4),
+                          child: Text(
+                            pembayaranku[index]['title']!,
+                            style: AppTextStyle.fontRegular(14),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 6),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "Rp 150.000",
-                                  style: AppTextStyle.fontBoldMidnightBlue(20),
-                                ),
-                                Text(
-                                  "Lunas",
-                                  style: AppTextStyle.font(18,
-                                      color: AppColors.forestGreen,
-                                      fw: FontWeight.w600),
-                                ),
-                              ],
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 100,
+                              child: Text(
+                                pembayaranku[index]['description']!,
+                                style: AppTextStyle.fontRegular(12),
+                              ),
                             ),
-                          )
-                        ],
+                            Padding(
+                              padding: const EdgeInsets.only(top: 6),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    pembayaranku[index]['price']!,
+                                    style:
+                                        AppTextStyle.fontBoldMidnightBlue(20),
+                                  ),
+                                  Text(
+                                    pembayaranku[index]['status']! == "Lunas"
+                                        ? "Lunas"
+                                        : "Belum Lunas",
+                                    style: AppTextStyle.font(18,
+                                        color: pembayaranku[index]['status']! ==
+                                                "Lunas"
+                                            ? AppColors.forestGreen
+                                            : AppColors.burningOrange,
+                                        fw: FontWeight.w600),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,4 +6,5 @@ class AppColors {
   static Color jetBlack = const Color.fromARGB(255, 49, 49, 49);
   static Color lavenderMist = const Color.fromARGB(255, 238, 238, 246);
   static Color forestGreen = const Color.fromARGB(255, 16, 109, 0);
+  static Color burningOrange = const Color.fromARGB(255, 192, 46, 0);
 }
